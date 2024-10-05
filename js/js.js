1fb4584322b2a7be4a32c99f3e2c74ab630fb1f7
Jairo-GitHub-Principal <jairocesa@outlook.com.br>
2024-10-05 00:57:00 -0300

    projeto optimizado

--- js/js.js
+++ js/js.js
@@ -18,10 +18,15 @@
 // implementa o evento onClick na tecla_clap e chama a função clp
 
 
-function tocarSom(idAudio) { // vamos receber o seletor por parametro
+function tocarSom(seletorAudio) { // vamos receber o seletor por parametro
     
-        let keyw = document.querySelector(idAudio); // funçã seleciona a tag que tem o audio do arquivo keyw que 
-        return keyw.play();                                                   // é o som "clap" e retorna um play no audio
+        const elemento = document.querySelector(seletorAudio); // funçã seleciona a tag que tem o audio do arquivo keyw que 
+       if(elemento != null   && elemento.localName ==='audio'){
+           
+                elemento.play();           
+        }else{
+            alert('elemento de audio não encontrado');
+        }
        
     }
 
@@ -32,7 +37,7 @@ function tocarSom(idAudio) { // vamos receber o seletor por parametro
 
         // selecionar em um lista de  classe que esta em uma tecla 
         const tecla = listaTeclas[contador];
-        const instrumento = tecla.classList[1]; // retorn a clsse de indice 1  
+        const instrumento = tecla.classList[1]; // retorn a clsse de indice 1 obs: aqui o valor definido pela let contador, é substituido por 1
         // montar o nome da classe que sera acessada na tag audio, pegaremos um nome de classe da tag button, e concatenaremos 
         // com um nome constante, que ao juntarmos teremos um nome especifica de uma classe existente na tag audio, 
         // possibilitando assim que nos acessemos a tag que queremos para executar o arquivo de audi que esta nela, e assim
@@ -42,13 +47,49 @@ function tocarSom(idAudio) { // vamos receber o seletor por parametro
         console.log(idAudio);
 
         
-        tecla.addEventListener('click',()=>{
+        tecla.addEventListener('click',()=>{ //ouvinte identifica  um evento de click em uma tecla, sera chamado a função tocarSom();
+            tocarSom(idAudio);
+           
+        });
+
+
+
+        // ouvinte identifica um evente de tecla pressionada e adiciona a classe "ativa" para o elemento html que esta selecionado e armazenado na variavel tecla 
+        tecla.addEventListener('keydown',function(event){ // o parametro passado para a função ela armazena as propriedades do evento que esta sendo ouvido nessa função 
+            console.log(event.code); // o event.code traz o atributo code, que por debaixo dos panos esta contido na evento de teclado keydown
+            if(event.code == 'Space'||event.code =='Enter'){
+            tecla.classList.add('ativa');
+            }
+        });
+        // ouvinte identifica um evente de tecla desapertada e remove a classe "ativa" para o elemento html que esta selecionado e armazenado na variavel tecla 
+        tecla.addEventListener('keyup',function(event){
+            console.log(event);
+            if(event.code == 'Space'||event.code =='Enter'){
+            tecla.classList.remove('ativa');
+            }
+        });
+
+       
+
+        //ou assim
+        /**
+         *  tecla.addEventListener('click',function(){
             tocarSom(idAudio);
         });
+         */
            
             
-       
+        // efeito active nas teclas 
+
+        
 
         contador++;
         console.log(contador)
     }
+
+
+    // teste de evento de click do mause
+    document.querySelector('.tecla').addEventListener('click', function(event) {
+        console.log(event); // Aqui 'event' contém todas as informações do evento de clique
+      });
+    
